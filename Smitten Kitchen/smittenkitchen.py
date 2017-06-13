d2609d436e4a1df6ecf2004e6fbf69f4369a9967
michaelsilverstein <msilver4@bu.edu>
2017-06-12 21:34:11 -0400

    Scraping smitten kitchen

---
+++ Smitten Kitchen/smittenkitchen.py
@@ -0,0 +1,17 @@
+import pandas as pd
+from bs4 import *
+import urllib
+import sys
+
+try:
+    url = sys.argv[1]
+except:
+    url = 'https://smittenkitchen.com/2017/06/crispy-spiced-lamb-and-lentils/'
+
+fh = urllib.urlopen(url)
+soup = BeautifulSoup(fh,'lxml')
+recipe = soup.title.string.encode('ascii','ignore')
+ingredients = [x.string.encode('ascii','ignore') for x in soup.find('div','jetpack-recipe-ingredients').find_all('li')]
+print recipe,url
+for x in ingredients:
+    print x
